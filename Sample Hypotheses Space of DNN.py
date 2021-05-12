c97755484cfa9e63c633ba79a8d667ec43956da1
dmarcondes <diegormmarcondes@gmail.com>
2021-05-12 12:09:24 -0300

    Data files and python code

--- Sample Hypotheses Space of DNN.py
+++ Sample Hypotheses Space of DNN.py
@@ -1,6 +1,6 @@
 #https://github.com/calebrob6/imagenet_validation
 #Importing packages
-import sys, os, time
+import sys, os, time, re, gc
 from pathlib import Path
 from glob import glob
 
@@ -17,6 +17,8 @@ import time
 
 import tensorflow as tf
 import tensorflow.keras as keras
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras import backend as K
 
 tf.__version__
 # Make sure GPU is available
@@ -44,23 +46,37 @@ def load_images(image_paths, returned_shard, n_shards=5):
     shard_size = len(image_paths) // n_shards
     sharded_image_paths = image_paths[returned_shard*shard_size:(returned_shard+1)*shard_size] if returned_shard < n_shards - 1 \
                      else image_paths[returned_shard*shard_size:]
-    images_list = np.zeros((len(sharded_image_paths), 224, 224, 3), dtype=np.uint8)
+    images_list = np.zeros((len(sharded_image_paths), 299, 299, 3), dtype=np.uint8)
     for i, image_path in enumerate(sharded_image_paths):
         # Load (in BGR channel order)
         image = cv2.imread(image_path)
         # Resize
         height, width, _ = image.shape
-        new_height = height * 256 // min(image.shape[:2])
-        new_width = width * 256 // min(image.shape[:2])
+        new_height = height * 300 // min(image.shape[:2])
+        new_width = width * 300 // min(image.shape[:2])
         image = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_CUBIC)
         # Crop
         height, width, _ = image.shape
-        startx = width//2 - (224//2)
-        starty = height//2 - (224//2)
-        image = image[starty:starty+224,startx:startx+224]
-        assert image.shape[0] == 224 and image.shape[1] == 224, (image.shape, height, width)
+        startx = width//2 - (299//2)
+        starty = height//2 - (299//2)
+        image = image[starty:starty+299,startx:startx+299]
+        assert image.shape[0] == 299 and image.shape[1] == 299, (image.shape, height, width)
         images_list[i, ...] = image[..., ::-1]
     return images_list
+
+#Top k accuracy
+def top_k_accuracy(y_true, y_pred, k=1, tf_enabled=True):
+    '''
+    Calculates top_k accuracy of predictions. Expects both y_true and y_pred to be one-hot encoded.
+    numpy implementation is from: https://github.com/chainer/chainer/issues/606
+    '''
+    if tf_enabled:
+        argsorted_y = tf.argsort(y_pred)[:,-k:]
+        matches = tf.cast(tf.math.reduce_any(tf.transpose(argsorted_y) == tf.argmax(y_true, axis=1, output_type=tf.int32), axis=0), tf.float32)
+        return tf.math.reduce_mean(matches).numpy()
+    else:
+        argsorted_y = np.argsort(y_pred)[:,-k:]
+        return np.any(argsorted_y.T == y_true.argmax(axis=1), axis=0).mean()
 
 #Paths to files
 path_imagenet_val_dataset = Path("/home/dmarcondes/ILSVRC2012_img_val/images/") # path/to/data/
@@ -102,7 +118,7 @@ np.save(str(path_imagenet_val_dataset/"y_val.npy"), y_val)
 image_paths = sorted(glob(str(dir_images/"*")))
 n_images = len(image_paths)
 n_images
-exist_shards = len(orted(glob(str(path_imagenet_val_dataset/"*")))) - 1
+exist_shards = len(sorted(glob(str(path_imagenet_val_dataset/"*")))) - 1
 
 #Dividing the images in 500 shards if it has not been done before
 n_shards = 500
@@ -125,18 +141,21 @@ if examples:
     n_cols = 3
     n_rows = 15 // n_cols
     figsize = (20, 20)
-
     indices = np.random.choice(x_val.shape[0], size=n_images2show, replace=False)
     images = x_val[indices] / 255.
-
     fig, ax = plt.subplots(figsize=figsize, nrows=n_rows, ncols=n_cols)
     for i, axi in enumerate(ax.flat):
         axi.imshow(images[i])
         label_index = (idx_shard - 1) * (n_images // n_shards) + indices[i]
         axi.set_title(keras_idx_to_name[y_val[label_index]], y=.9, fontdict={'fontweight':'bold'}, pad=10)
         axi.set_axis_off()
-
     plt.show()
+
+#Load data
+x_val_paths = glob(str(path_imagenet_val_dataset / "x_val*.npy"))
+x_val_paths.sort(key=lambda f: int(re.sub('\D', '', f)))
+y_val = np.load(str(path_imagenet_val_dataset / "y_val.npy"))
+y_val_one_hot = to_categorical(y_val, 1000)
 
 #Model
 model = tf.keras.applications.Xception(include_top=True,weights="imagenet")
@@ -153,30 +172,27 @@ for i in range(depth):
         if i > 0:
             for j in range(i):
                 modelRandom.layers[j].set_weights(model.layers[j].get_weights())
-        top10 = 0
-        top5 = 0
-        top1 = 0
-        for f in range(n_shards):
-            print("Shard " + str(f))
-            time1 = time.time()
-            x_val = np.load(str(path_imagenet_val_dataset/"x_val_{}.npy").format(f+1))
-            y_val = np.load(str(path_imagenet_val_dataset/"y_val_{}.npy").format(f+1))
-            pred = tf.keras.applications.imagenet_utils.decode_predictions(modelRandom.predict_on_batch(x_val), top=10)
-            pred = np.array(pred)
-            pred = pred[:,:,0]
-            print("Predict "+str(time.time()-time1))
-            time1 = time.time()
-            for k in range(len(y_val)):
-                top10 = top10 + int(y_val[k] in pred[k])
-                top5 = top5 + int(y_val[k] in pred[k][:5])
-                top1 = top1 + int(y_val[k] in pred[k][0])
-            print("Calculate topX "+str(time.time()-time1))
-        top10 = top10/n_images
-        top5 = top5/n_images
-        top1 = top1/n_images
-        err10 = np.concatenate([err10,[top10]])
-        err5 = np.concatenate([err5,[top5]])
-        err1 = np.concatenate([err1,[top1]])
+y_pred = None
+for k, x_val_path in enumerate(x_val_paths):
+    x_val = np.load(x_val_path).astype('float32') # loaded as RGB
+    y_pred_sharded = modelRandom.predict(x_val, verbose=0, use_multiprocessing=True, batch_size=64, callbacks=None)
+    try:
+        y_pred = np.concatenate([y_pred, y_pred_sharded])
+    except ValueError:
+        y_pred = y_pred_sharded
+    del x_val
+    gc.collect()
+    completed_percentage = (k + 1) * 100 / len(x_val_paths)
+    if completed_percentage % 5 == 0:
+        print("{:5.1f}% completed.".format(completed_percentage))
+
+
+    top10 = top_k_accuracy(y_val_one_hot, y_pred, k=10)
+    top5 = top_k_accuracy(y_val_one_hot, y_pred, k=5)
+    top1 = top_k_accuracy(y_val_one_hot, y_pred, k=1)
+    err10 = np.concatenate([err10,[top10]])
+    err5 = np.concatenate([err5,[top5]])
+    err1 = np.concatenate([err1,[top1]])
     tab = tab + [(i,np.mean(err10),st.stdev(err10),np.percentile(err10,0),np.percentile(err10,25),np.percentile(err10,50),np.percentile(err10,75),np.percentile(err10,100),
     np.mean(err5),st.stdev(err5),np.percentile(err5,0),np.percentile(err5,25),np.percentile(err5,50),np.percentile(err5,75),np.percentile(err5,100)),
     np.mean(err1),st.stdev(err1),np.percentile(err1,0),np.percentile(err1,25),np.percentile(err1,50),np.percentile(err1,75),np.percentile(err1,100)]
