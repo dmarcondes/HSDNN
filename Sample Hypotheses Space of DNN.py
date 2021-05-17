a0b34a4d9945ae5bafc6a889d60ff980236d1697
dmarcondes <diegormmarcondes@gmail.com>
2021-05-17 12:01:35 -0300

    Main code

--- Sample Hypotheses Space of DNN.py
+++ Sample Hypotheses Space of DNN.py
@@ -4,6 +4,7 @@ import sys, os, time, re, gc
 from pathlib import Path
 from glob import glob
 import json
+from tqdm import tqdm
 
 # Select GPU
 #os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
@@ -21,7 +22,6 @@ import tensorflow as tf
 import tensorflow.keras as keras
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras import backend as K
-tf.keras.backend.set_learning_phase(0)
 from tensorflow.keras.backend import clear_session
 
 #tf.__version__
@@ -171,20 +171,20 @@ model = tf.keras.applications.MobileNetV2(include_top=True,weights="imagenet")
 depth = len(model.layers)
 tab = np.array([])
 
-for i in range(depth):
+for i in tqdm(range(depth)):
     if len(model.layers[i].get_weights()) > 0 or i == 0:
         print("layer "+str(i+1)+" from "+str(depth))
         err10 = np.array([])
         err5 = np.array([])
         err1 = np.array([])
-        for r in range(100):
+        for r in tqdm(range(100)):
             print("Repetition " + str(r+1))
             modelRandom = tf.keras.applications.MobileNetV2(include_top=True,weights = None)
             m1 = tf.keras.applications.MobileNetV2(include_top=True,weights = None)
             if i > 0:
                 for j in range(i+1):
                     modelRandom.layers[j].set_weights(model.layers[j].get_weights())
-            y_pred = modelRandom.predict(x_val, verbose=1, use_multiprocessing=True, batch_size=128, callbacks=None)
+            y_pred = modelRandom.predict(x_val, verbose=0, use_multiprocessing=True, batch_size=128, callbacks=None)
             m10 = top_k_accuracy(y_val_one_hot, y_pred, k=10)
             m5 = top_k_accuracy(y_val_one_hot, y_pred, k=5)
             m1 = top_k_accuracy(y_val_one_hot, y_pred, k=1)
@@ -202,7 +202,7 @@ for i in range(depth):
             tab = np.array([i,np.mean(err10),st.stdev(err10),np.percentile(err10,0),np.percentile(err10,25),np.percentile(err10,50),np.percentile(err10,75),np.percentile(err10,100),
             np.mean(err5),st.stdev(err5),np.percentile(err5,0),np.percentile(err5,25),np.percentile(err5,50),np.percentile(err5,75),np.percentile(err5,100),
             np.mean(err1),st.stdev(err1),np.percentile(err1,0),np.percentile(err1,25),np.percentile(err1,50),np.percentile(err1,75),np.percentile(err1,100)])
-        np.save("results2.npy",tab)
+        np.save("results.npy",tab)
 
 tab = np.load("results.npy",allow_pickle=True)
 df = pd.DataFrame(data=tab.reshape((-1,22)))
